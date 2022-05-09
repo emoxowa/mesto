e29d56aa16f9d2085ddd267dc9c6600cee071f26
Evgeniia <100534408+emoxowa@users.noreply.github.com>
2022-05-09 20:36:52 +0300

    fix: hotfix after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,31 @@
-const openButton = document.querySelector(".button_type_edit-open");
+const openButton = document.querySelector(".button_type_edit");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close-icon");
 const saveButton = document.querySelector(".button_type_save");
-
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
-
 let nameInput = document.querySelector("#name");
 let jobInput = document.querySelector("#job");
-
 
 function saveProfile(evt) {
   evt.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  closePopup();
+}
+
+function togglePopup() {
+  if (!popup.classList.contains("popup_opened")) {
+    popup.classList.add("popup_opened");
+    nameInput.value = profileName.innerText;
+    jobInput.value = profileJob.innerText;
+  }
+}
+
+function closePopup() {
   popup.classList.remove("popup_opened");
 }
 
 saveButton.addEventListener("click", saveProfile);
-openButton.addEventListener('click', togglePopup);
-closeButton.addEventListener("click", togglePopup);
-
-
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
-  nameInput.value = profileName.innerText;
-  jobInput.value = profileJob.innerText;
-}
-
+openButton.addEventListener("click", togglePopup);
+closeButton.addEventListener("click", closePopup);
