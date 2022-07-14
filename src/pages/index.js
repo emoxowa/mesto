54301b93a412878762e1a887d308973e56eaca87
Evgeniia <Kochetkovaev11@gmail.com>
2022-07-14 20:39:46 +0300

    install webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import "./index.css";
+
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
